763f6e0a8adb682d0f7f87a31ebc3716fc62a15c
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2023-11-04 23:35:05 -0400

    Added check in/out and lock

--- alls_keychip/alls_keychip.ino
+++ alls_keychip/alls_keychip.ino
@@ -21,6 +21,8 @@ void ledColor(int r, int g, int b) {
 
 char c;
 static String receivedMessage = "";
+int open_disks[10] = {0,0,0,0,0,0,0,0,0,0}; 
+bool deviceReady = false;
 
 void loop() {
   if (Serial.available()) {
@@ -38,46 +40,73 @@ void loop() {
               ledColor(0,255,0);
               delay(1000);
               ledColor(0,64,0);
-            } else if (command == "DECRYPT") {
-              ledColor(0,0,255);
-              int appIDIndex = receivedMessage.indexOf("//", commandIndex + 2);
-              String appID = receivedMessage.substring(commandIndex + 2, appIDIndex);
-
-              if (appID == applicationID) {
-                int appCheckIndex = receivedMessage.indexOf("//", appIDIndex + 2);
-                String appCheck = receivedMessage.substring(appIDIndex + 2, appCheckIndex);
-                int appDriveIndex = receivedMessage.indexOf("//", appCheckIndex + 2);
-                String appDrive = receivedMessage.substring(appCheckIndex + 2, appDriveIndex);
-
-                String inputString = applicationID;
-                inputString += " Copyright(C)SEGA ";
-                inputString += appDrive;
-                inputString += " DISK";
-                inputString += appCheck;
-                inputString += " 0x0001";
-                SHA256.beginHmac(applicationKey);
-                SHA256.print(inputString);
-                SHA256.endHmac();
-
-                while (SHA256.available()) {
-                  byte b = SHA256.read();
-
-                  if (b < 16) {
-                    Serial.print("0");
-                  }
-
-                  Serial.print(b, HEX);
-                }
-                Serial.print("0");
-                delay(2000);
+            } if (command == "CHECK_IN") {
+              if (deviceReady == false) {
+                Serial.println("READY");
+                deviceReady = true;
+                ledColor(0,255,0);
+                delay(1000);
                 ledColor(0,64,0);
               } else {
-                Serial.print("0");
-                ledColor(255,0,0);
-                delay(2000);
-                ledColor(64,0,0);
+                lockDevice();
               }
-              Serial.println();
+            } if (command == "CHECK_OUT") {
+              if (deviceReady == true) {
+                int i = 0;
+                while (i <= 10) {
+                  open_disks[i] = 0;
+                  i++;
+                }
+                deviceReady = false;
+                Serial.println("LOCKED");
+                ledColor(0,255,0);
+                delay(1000);
+                ledColor(0,64,0);
+              } else  {
+                lockDevice();
+              }
+            } else if (command == "DECRYPT") {
+              if (deviceReady == true) {
+                ledColor(0,0,255);
+                int appIDIndex = receivedMessage.indexOf("//", commandIndex + 2);
+                String appID = receivedMessage.substring(commandIndex + 2, appIDIndex);
+                  int appCheckIndex = receivedMessage.indexOf("//", appIDIndex + 2);
+                  String appCheck = receivedMessage.substring(appIDIndex + 2, appCheckIndex);
+                  int appDriveIndex = receivedMessage.indexOf("//", appCheckIndex + 2);
+                  String appDriveStr = receivedMessage.substring(appCheckIndex + 2, appDriveIndex);
+                  int appDrive = appDriveStr.toInt();
+
+                if (appID == applicationID && open_disks[appDrive] == 0) {
+                  String inputString = applicationID;
+                  inputString += " Copyright(C)SEGA ";
+                  inputString += appDrive;
+                  inputString += " DISK";
+                  inputString += appCheck;
+                  inputString += " 0x0001";
+                  SHA256.beginHmac(applicationKey);
+                  SHA256.print(inputString);
+                  SHA256.endHmac();
+
+                  while (SHA256.available()) {
+                    byte b = SHA256.read();
+
+                    if (b < 16) {
+                      Serial.print("0");
+                    }
+
+                    Serial.print(b, HEX);
+                  }
+                  Serial.print("0");
+                  Serial.println();
+                  open_disks[appDrive] = 1;
+                  delay(2000);
+                  ledColor(0,64,0);
+                } else {
+                  lockDevice();
+                }
+              } else  {
+                lockDevice();
+              }
             }
           }
         }
@@ -86,5 +115,14 @@ void loop() {
     } else {
       receivedMessage += c;
     }
+  }
+}
+
+void lockDevice() {
+  for(;;) {
+    ledColor(255,0,0);
+    delay(100);
+    ledColor(64,0,0);
+    delay(100);
   }
 }
